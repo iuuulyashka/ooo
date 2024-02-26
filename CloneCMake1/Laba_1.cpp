22e43a6b3beb7adf7295f3250e03c3dc839d702f
iuuulyashka <145469118+iuuulyashka@users.noreply.github.com>
2024-02-26 21:29:42 +0200

    Add files via upload

---
+++ CloneCMake1/Laba_1.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    float radius;
+    float pi = 3.14;
+    double dlina;
+
+    std::cout << "Radius:";
+    std::cin >> radius;
+    dlina = pi * 2 * radius;
+    std::cout << "Dlina:" << dlina;
+    return 0;
+}
+
